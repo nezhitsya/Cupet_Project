67dc492c5b3860c412c6f74d08c8e976a5b55c7f
nezhitsya <huyn9295@naver.com>
2021-01-10 00:56:36 +0900

    set up logo

--- app/src/main/java/com/example/cupet/MainActivity.kt
+++ app/src/main/java/com/example/cupet/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.cupet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var user = FirebaseAuth.getInstance().currentUser
+
+        if (user != null) {
+            val homeIntent = Intent(this, HomeActivity::class.java)
+            startActivity(homeIntent)
+        } else {
+            val loginActivity = Intent(this, LoginActivity::class.java)
+            startActivity(login)
+        }
     }
 }
