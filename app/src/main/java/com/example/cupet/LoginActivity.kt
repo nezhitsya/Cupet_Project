a7278179c7d4b8b4e11122d6c9395ca3df7ce1c4
nezhitsya <huyn9295@naver.com>
2021-01-11 11:48:47 +0900

    set up login

--- app/src/main/java/com/example/cupet/LoginActivity.kt
+++ app/src/main/java/com/example/cupet/LoginActivity.kt
@@ -3,7 +3,9 @@ package com.example.cupet
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -22,8 +24,19 @@ class LoginActivity : AppCompatActivity() {
         }
 
         login.setOnClickListener{
-
+            doLogin()
         }
     }
 
+    private fun doLogin() {
+        auth.signInWithEmailAndPassword(emailText.text.toString(),
+            passwordText.text.toString()).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                startActivity(Intent(this, HomeActivity::class.java))
+                finish()
+            } else {
+                Toast.makeText(baseContext, "다시 확인해주세요.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
