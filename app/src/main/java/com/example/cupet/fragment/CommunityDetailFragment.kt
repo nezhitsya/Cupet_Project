eaae6fab57c680a4c2862986730cfd1e2ffd4a16
nezhitsya <huyn9295@naver.com>
2021-01-20 01:35:10 +0900

    set community detail

--- app/src/main/java/com/example/cupet/fragment/CommunityDetailFragment.kt
+++ app/src/main/java/com/example/cupet/fragment/CommunityDetailFragment.kt
@@ -10,12 +10,21 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 import com.example.cupet.R
 import com.example.cupet.model.Post
+import com.example.cupet.model.User
 import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.fragment_community_detail.*
+import java.text.DateFormat
+import java.text.SimpleDateFormat
 
 class CommunityDetailFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
 
     lateinit var mReference: DatabaseReference
     lateinit var postid: String
@@ -39,6 +48,17 @@ class CommunityDetailFragment : Fragment() {
         val preferences: SharedPreferences = context!!.getSharedPreferences("PREFS", Context.MODE_PRIVATE)
         postid = preferences.getString("postid", "none").toString()
 
+        recyclerView = view.findViewById(R.id.recycler_view)
+        recyclerView.setHasFixedSize(true)
+        val linearLayoutManager = LinearLayoutManager(context)
+        linearLayoutManager.reverseLayout = true
+        linearLayoutManager.stackFromEnd = true
+        recyclerView.layoutManager = linearLayoutManager
+
+        bookmark.setOnClickListener {
+            bookmark.setImageResource(R.drawable.ic_bookmarked)
+        }
+
         postInfo()
 
         return view
@@ -48,12 +68,38 @@ class CommunityDetailFragment : Fragment() {
 
         mReference = FirebaseDatabase.getInstance().getReference("Posts").child(postid)
 
-        mReference.addListenerForSingleValueEvent(object: ValueEventListener {
+        mReference.addValueEventListener(object: ValueEventListener {
 
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 val post: Post? = dataSnapshot.getValue(Post::class.java)
                 post?.let {
+                    title.text = post.title
+                    description.text = post.description
+                    var df: DateFormat = SimpleDateFormat("yy.MM.dd  hh:mm")
+                    time.text = df.format(post.time)
+                    var publisher = post.publisher
+                    if (publisher != null) {
+                        publisherInfo(publisher)
+                    }
+                }
+            }
 
+            override fun onCancelled(dataSnapshot: DatabaseError) {
+
+            }
+        })
+    }
+
+    private fun publisherInfo(publisher: String) {
+
+        var reference: DatabaseReference = FirebaseDatabase.getInstance().getReference("Users").child(publisher)
+
+        reference.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val user: User? = dataSnapshot.getValue(User::class.java)
+                user?.let {
+                    nickname.text = user.nickname
+                    Glide.with(context!!).load(user.profile).into(profile)
                 }
             }
 
