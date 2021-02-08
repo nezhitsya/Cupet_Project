e6f22fc00e23abc7009dbf1508edb964bf9a6ace
nezhitsya <huyn9295@naver.com>
2021-02-08 17:39:59 +0900

    set up write

--- app/src/main/java/com/example/cupet/fragment/WriteFragment.kt
+++ app/src/main/java/com/example/cupet/fragment/WriteFragment.kt
@@ -2,7 +2,6 @@ package com.example.cupet.fragment
 
 import android.app.Activity
 import android.content.ContentResolver
-import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -14,11 +13,9 @@ import android.webkit.MimeTypeMap
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 import com.example.cupet.R
-import com.example.cupet.model.Post
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -67,7 +64,8 @@ class WriteFragment : Fragment() {
     }
 
     private fun post() {
-        var description = description.text.toString()
+        var description1 = description1.text.toString()
+        var description2 = description2.text.toString()
         var title = title.text.toString()
         var reference: DatabaseReference = FirebaseDatabase.getInstance().getReference("Posts")
 
@@ -75,12 +73,14 @@ class WriteFragment : Fragment() {
 
         val hashMap: HashMap<String, Any> = HashMap()
         hashMap["postid"] = postid
-        hashMap["description"] = description
+        hashMap["description1"] = description1
+        hashMap["description2"] = description2
         hashMap["title"] = title
         hashMap["time"] = System.currentTimeMillis()
         hashMap["publisher"] = FirebaseAuth.getInstance().currentUser!!.uid
 
         reference.child(postid).setValue(hashMap)
+        uploadImage()
 
         activity!!.supportFragmentManager.beginTransaction().replace(R.id.fragment_container, CommunityFragment()).addToBackStack(null).commit()
     }
@@ -109,6 +109,7 @@ class WriteFragment : Fragment() {
                     val reference: DatabaseReference = FirebaseDatabase.getInstance().getReference("Posts").child(postid)
                     var hashMap: HashMap<String, Any> = HashMap()
                     hashMap.put("postimage", url)
+                    Glide.with(context!!).load(url).into(photo)
                     reference.updateChildren(hashMap)
                 }
             }
@@ -125,7 +126,7 @@ class WriteFragment : Fragment() {
             var result: CropImage.ActivityResult  = CropImage.getActivityResult(data)
             mImageUri = result.uri
 
-            uploadImage()
+            post()
         } else {
             Toast.makeText(context, "다시 시도해주세요.", Toast.LENGTH_SHORT).show();
         }
